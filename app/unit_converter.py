7711d7a988e951e4e3977c387fb9d69ff7c2e135
George Mathew <geopm@outlook.com>
2024-01-14 20:01:14 +0000

    unit converter added

---
+++ app/unit_converter.py
@@ -0,0 +1,12 @@
+def bar_pascal(input):
+    result = input * 1e5
+    return result
+
+def celsius_kelvin(input):
+    result = input + 273.15
+    return result
+
+# add others
+# F to K
+# psi, MPa, to pa
+# reverse converters
