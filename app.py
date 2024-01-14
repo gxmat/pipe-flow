b02e6b75ced14f92f5220a5f5b79af9d5db96160
George Mathew <geopm@outlook.com>
2024-01-14 16:53:13 +0000

    basic flask webapp setup

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+        req = request.form["fluid_name"]
+        return render_template("index.html", result=req)
+    else:
+        return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
