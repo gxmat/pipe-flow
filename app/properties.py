3c89c55cc2bbb4293722c61be49b4477537b6f8c
George Mathew <geopm@outlook.com>
2024-01-14 21:40:05 +0000

    coolprop module setup

--- app/properties.py
+++ app/properties.py
@@ -1,19 +1,37 @@
 import CoolProp.CoolProp as cp
 import unit_converter as uc
 
-# Units in SI - Pascal, bar
+# PropsSI uses SI Units https://en.wikipedia.org/wiki/International_System_of_Units 
+# PropsSI available properties http://www.coolprop.org/coolprop/HighLevelAPI.html#table-of-string-inputs-to-propssi-function 
 
-temperature = 100
+class fluid:
+    def __init__(self, name, temperature, pressure):
+        self.name = name
+        self.temperature = temperature
+        self.pressure = pressure
+
+    # Only getter needed here since we don't directly set any properties
+    @property
+    def properties(self):
+        self.density = cp.PropsSI('D','T',self.temperature,'P',self.pressure,self.name) # kg/m3
+        self.dynamic_viscosity = cp.PropsSI('V','T',self.temperature,'P',self.pressure,self.name) # Pa.s
+        self.specific_heat = cp.PropsSI('C','T',self.temperature,'P',self.pressure,self.name) # J/kg.K
+        self.thermal_conductivity = cp.PropsSI('CONDUCTIVITY','T',self.temperature,'P',self.pressure,self.name) # W/m.K
+        self.pr_number = cp.PropsSI('PRANDTL','T',self.temperature,'P',self.pressure,self.name)
+        self.phase = cp.PhaseSI('T',self.temperature,'P',self.pressure,self.name)
+        self.temperature_saturation = cp.PropsSI('T','P',self.pressure,'Q',0,self.name) # K
+
+        return self
+
+# Remove test stuff below
+
+temperature = 200
 pressure = 1
+name = "Water"
 
 temperature_conv = uc.celsius_kelvin(temperature)
 pressure_conv = uc.bar_pascal(pressure)
 
-# PropsSI uses SI Units https://en.wikipedia.org/wiki/International_System_of_Units 
-# PropsSI available properties http://www.coolprop.org/coolprop/HighLevelAPI.html#table-of-string-inputs-to-propssi-function 
-
-def properties
-
-
-
+fluid1 = fluid(name, temperature_conv, pressure_conv)
+fluid2 = fluid(name, temperature_conv-100, pressure_conv)
 a = 1
