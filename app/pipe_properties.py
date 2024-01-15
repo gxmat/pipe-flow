cc652b1113a4187bf72c7f7827eec16c5b9fb030
George Mathew <geopm@outlook.com>
2024-01-15 22:11:11 +0000

    upload

---
+++ app/pipe_properties.py
@@ -0,0 +1,15 @@
+from math import pi
+
+# Class containing details of pipe section
+
+class pipe:
+    def __init__(self, od, thickness, length, roughness):
+        self.od = od # m
+        self.thickness = thickness # m
+        self.length = length # m
+        self.roughness = roughness # m
+        self.id = self.od - (2 * self.thickness) # m
+        self.id_wall_area = pi * self.id * self.length # m2
+        self.id_cs_area = 0.25 * pi * (self.id ** 2) # m2
+        self.relative_roughness = self.roughness / self.id
+        self.heatflux = 0.0 # W/m2
