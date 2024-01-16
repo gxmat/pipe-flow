3562b7fe13ee42a11bcff9db809c1ba12b3e0821
George Mathew <geopm@outlook.com>
2024-01-16 11:17:50 +0000

    simplified and now excludes heat and power

--- app/pipe_properties.py
+++ app/pipe_properties.py
@@ -1,7 +1,6 @@
 from math import pi
 
 # Class containing details of pipe section
-
 class pipe:
     def __init__(self, od, thickness, length, roughness):
         self.od = od # m
@@ -12,4 +11,3 @@ class pipe:
         self.id_wall_area = pi * self.id * self.length # m2
         self.id_cs_area = 0.25 * pi * (self.id ** 2) # m2
         self.relative_roughness = self.roughness / self.id
-        self.heatflux = 0.0 # W/m2
